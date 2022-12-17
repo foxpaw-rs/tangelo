73b482e338eabe8993a376c3646567eaa3a1d98e
Paul Busby <paul.adam.busby@gmail.com>
2022-12-17 22:38:10 +1100

    Simple Workflow + Stub methods created for height, size and width.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,97 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Todo(Paul): Crate Documentation once 0.1 complete.
+//! * Include a note about the crate is target OS specific
+//!
+//! # Usage
+//!
+//! # Examples
+//!
+//! For further detailed examples, refer to the documentation.
+
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+
+/// Tangelo.
+///
+/// The Tangelo struct that is used to interact with the terminal.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Tangelo {}
+
+impl Tangelo {
+    /// Get the current Tangelo.
+    ///
+    /// Create a new Tangelo object for the current terminal. Will return None if
+    /// the currently active window is not a terminal.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use tangelo::Tangelo;
+    ///
+    /// let tangelo = Tangelo::current().unwrap();
+    /// ```
+    pub fn current() -> Option<Self> {
+        Some(Tangelo {})
+    }
+
+    /// Determine the terminal height.
+    ///
+    /// Retrieve the termnal height.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use tangelo::Tangelo;
+    ///
+    ///
+    /// let tangelo = Tangelo::current().unwrap();
+    /// let height = tangelo.get_height();
+    /// ```
+    pub fn get_height(&self) -> u16 {
+        0
+    }
+
+    /// Determine the terminal size.
+    ///
+    /// Retrieve the termnal size.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use tangelo::Tangelo;
+    ///
+    /// let tangelo = Tangelo::current().unwrap();
+    /// let (height, width) = tangelo.get_size();
+    /// ```
+    pub fn get_size(&self) -> (u16, u16) {
+        (0, 0)
+    }
+
+    /// Determine the terminal width.
+    ///
+    /// Retrieve the termnal width.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use tangelo::Tangelo;
+    ///
+    /// let tangelo = Tangelo::current().unwrap();
+    /// let width = tangelo.get_width();
+    /// ```
+    pub fn get_width(&self) -> u16 {
+        0
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
+    /// Tangelo::current must create as per struct initialisation.
+    ///
+    /// The current method on Tangelo must create an object as per the struct
+    /// initialiser syntax.
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn tangelo_current() {
+        let expected = Tangelo {};
+        let actual = Tangelo::current().unwrap();
+        assert_eq!(expected, actual);
     }
 }
